3c90ce7ed38ada5b1468fb8dd2c283cb71b255e6
Bob Belderbos <bobbelderbos@gmail.com>
2024-07-04 14:19:25 +0200

    order questions, automate slug

--- new_post.sh
+++ new_post.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-# Prompt for the slug and title
-read -p "Enter the slug (e.g., my-new-post): " slug
+# Prompt for the title
 read -p "Enter the title: " title
+
+# Convert the title to a slug
+slug=$(echo "$title" | tr '[:upper:]' '[:lower:]' | tr -s '[:space:]' '-' | tr -cd '[:alnum:]-' | sed 's/-$//')
+
+# Prompt for the slug with the default value
+read -p "Enter the slug (default: $slug): " input_slug
+
+# Use the user-provided slug or the default
+slug=${input_slug:-$slug}
 
 # Get today's date
 date=$(date +"%Y-%m-%d")
